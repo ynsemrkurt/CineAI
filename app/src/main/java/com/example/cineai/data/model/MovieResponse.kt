5259c15b011c35b4c69b911b20eb82fe43b0655b
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-02 12:52:27 +0300

    feat: Add MovieResponse data class

---
+++ app/src/main/java/com/example/cineai/data/model/MovieResponse.kt
@@ -0,0 +1,16 @@
+package com.example.cineai.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+    @SerializedName("results")
+    val results: List<Movie>
+)
+
+data class Movie(
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("poster_path")
+    val posterPath: String
+)
+
