16a488d0af4fa334bd373c617d59188dd75abd32
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-02 11:55:13 +0300

    feat: Add initial resources

---
+++ app/src/main/java/com/example/cineai/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cineai.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cineai.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
