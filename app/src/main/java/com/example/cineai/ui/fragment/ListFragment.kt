fd94824dd3020775ec6510f4af4025bc6ed97e3f
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-02 12:53:08 +0300

    feat: Add ListFragment UI

--- app/src/main/java/com/example/cineai/ui/fragment/ListFragment.kt
+++ app/src/main/java/com/example/cineai/ui/fragment/ListFragment.kt
@@ -5,13 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.cineai.R
+import androidx.fragment.app.viewModels
+import com.example.cineai.databinding.FragmentListBinding
+import com.example.cineai.ui.adapter.MovieAdapter
+import com.example.cineai.ui.viewmodel.MovieViewModel
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+    private val movieViewModel: MovieViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        movieViewModel.fetchPopularMovies()
+
+        movieViewModel.movies.observe(viewLifecycleOwner) { movies ->
+            val adapter = MovieAdapter(movies)
+            binding.recyclerViewList.apply {
+                this.adapter = adapter
+            }
+        }
     }
 }
