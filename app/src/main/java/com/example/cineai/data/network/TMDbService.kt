b1b3f57e79339ff11098e194fcce91fd53820ea9
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-07 18:34:08 +0300

    refactor: Extract API endpoints to constants in TMDbService

--- app/src/main/java/com/example/cineai/data/network/TMDbService.kt
+++ app/src/main/java/com/example/cineai/data/network/TMDbService.kt
@@ -5,15 +5,20 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TMDbService {
-    @GET("movie/popular")
+    @GET(popularMovies)
     suspend fun getPopularMovies(@Query("page") page: Int): MovieResponse
 
-    @GET("movie/top_rated")
+    @GET(topRatedMovies)
     suspend fun getTopRatedMovies(@Query("page") page: Int): MovieResponse
 
-    @GET("movie/now_playing")
+    @GET(nowPlayingMovies)
     suspend fun getNowPlayingMovies(@Query("page") page: Int): MovieResponse
 
-    @GET("movie/upcoming")
+    @GET(upcomingMovies)
     suspend fun getUpcomingMovies(@Query("page") page: Int): MovieResponse
 }
+
+private const val popularMovies = "movie/popular"
+private const val topRatedMovies = "movie/top_rated"
+private const val nowPlayingMovies = "movie/now_playing"
+private const val upcomingMovies = "movie/upcoming"
