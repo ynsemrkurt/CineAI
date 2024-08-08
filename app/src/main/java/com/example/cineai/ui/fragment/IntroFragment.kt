adfddc6b25735acc49b094c5efedae4a0bf68c05
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-08 14:40:50 +0300

    feat: Add IntroFragment UI and navigation

--- app/src/main/java/com/example/cineai/ui/fragment/IntroFragment.kt
+++ app/src/main/java/com/example/cineai/ui/fragment/IntroFragment.kt
@@ -6,13 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cineai.R
+import com.example.cineai.databinding.FragmentIntroBinding
 
 class IntroFragment : Fragment() {
+
+    private lateinit var binding: FragmentIntroBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_intro, container, false)
+    ): View {
+        binding = FragmentIntroBinding.inflate(inflater)
+        return binding.root
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonRegister.setOnClickListener{
+            openFragment(RegisterFragment())
+        }
+
+        binding.buttonLogin.setOnClickListener{
+            openFragment(LoginFragment())
+        }
+    }
+
+    private fun openFragment(fragment: Fragment){
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
