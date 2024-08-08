c590af9b528af9fbfde112511c512313b1f323f2
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-08 12:04:42 +0300

    feat: Add LoginActivity and IntroFragment

---
+++ app/src/main/java/com/example/cineai/ui/activity/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cineai.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cineai.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
