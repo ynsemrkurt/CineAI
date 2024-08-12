3aad44889653f950e4910c2f831de6b304a1c904
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-12 18:17:30 +0300

    feat: Implement bottom navigation bar

--- app/src/main/java/com/example/cineai/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/cineai/ui/activity/MainActivity.kt
@@ -3,13 +3,11 @@ package com.example.cineai.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.cineai.R
 import com.example.cineai.databinding.ActivityMainBinding
 import com.example.cineai.ui.classes.MovieCategory
 import com.example.cineai.ui.fragment.BaseMovieFragment
-import com.google.android.material.tabs.TabLayoutMediator
+import com.example.cineai.ui.fragment.MovieFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,36 +17,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setupViewPager()
+        setNavigationBar()
     }
 
-    private fun setupViewPager() {
-        binding.viewPagerMovie.adapter = MoviePagerAdapter(this)
-        TabLayoutMediator(binding.tabLayout, binding.viewPagerMovie) { tab, position ->
-            tab.text = when (position) {
-                0 -> getString(R.string.popular)
-                1 -> getString(R.string.top_rated)
-                2 -> getString(R.string.now_playing)
-                3 -> getString(R.string.upcoming)
-                else -> null
+    private fun setNavigationBar() {
+        binding.navigationBarView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_item_home -> {
+                    openFragment(MovieFragment())
+                    true
+                }
+
+                R.id.menu_item_favorite -> {
+                    openFragment(BaseMovieFragment.newInstance(MovieCategory.FAVORITE))
+                    true
+                }
+
+                else -> false
             }
-        }.attach()
+        }
     }
 
-    class MoviePagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
-        override fun getItemCount(): Int {
-            return 4
-        }
-
-        override fun createFragment(position: Int): Fragment {
-            return when (position) {
-                0 -> BaseMovieFragment.newInstance(MovieCategory.POPULAR)
-                1 -> BaseMovieFragment.newInstance(MovieCategory.TOP_RATED)
-                2 -> BaseMovieFragment.newInstance(MovieCategory.NOW_PLAYING)
-                3 -> BaseMovieFragment.newInstance(MovieCategory.UPCOMING)
-                else -> BaseMovieFragment.newInstance(MovieCategory.POPULAR)
-            }
-        }
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
