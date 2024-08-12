81028369c222813092cf83399ad0168160c38b64
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-12 18:17:42 +0300

    refactor: Remove unused imports in LoginActivity

--- app/src/main/java/com/example/cineai/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/cineai/ui/activity/LoginActivity.kt
@@ -2,10 +2,7 @@ package com.example.cineai.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.cineai.R
 import com.google.firebase.auth.FirebaseAuth
 
