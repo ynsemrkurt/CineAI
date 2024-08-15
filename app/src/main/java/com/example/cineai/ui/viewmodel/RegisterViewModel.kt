869dce3d8aa9333053e5688f6b1e2e486c40e3b7
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-15 10:31:08 +0300

    refactor: Simplify error handling in registration

--- app/src/main/java/com/example/cineai/ui/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/cineai/ui/viewmodel/RegisterViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.cineai.ui.viewmodel
 
-import android.content.Context
+import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.cineai.R
 import com.example.cineai.data.model.User
+import com.example.cineai.ui.classes.isValidEmail
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -14,83 +15,78 @@ class RegisterViewModel : ViewModel() {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-    private val _registrationStatus = MutableLiveData<String>()
-    val registrationStatus: LiveData<String> get() = _registrationStatus
+    private val _registrationStatus = MutableLiveData<@receiver:StringRes Int>()
+    val registrationStatus: LiveData<Int> get() = _registrationStatus
 
-    fun registerUser(username: String, email: String, password: String, context: Context) {
+    fun registerUser(username: String, email: String, password: String) {
         if (username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
-            validateInputs(username, email, password, context)
+            validateInputs(username, email, password)
         } else {
-            _registrationStatus.value = context.getString(R.string.invalid_input)
+            _registrationStatus.value = R.string.invalid_input
         }
     }
 
     private fun validateInputs(
         username: String,
         email: String,
-        password: String,
-        context: Context
+        password: String
     ) {
         when {
             username.length < 3 -> {
-                _registrationStatus.value = context.getString(R.string.username_characters_error)
+                _registrationStatus.value = R.string.username_characters_error
             }
 
-            email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
-                _registrationStatus.value = context.getString(R.string.invalid_email_error)
+            !email.isValidEmail() -> {
+                _registrationStatus.value = R.string.invalid_email_error
             }
 
             password.length < 6 -> {
-                _registrationStatus.value = context.getString(R.string.password_characters_error)
+                _registrationStatus.value = R.string.password_characters_error
             }
 
-            else -> checkUsernameAvailability(username, email, password, context)
+            else -> checkUsernameAvailability(username, email, password)
         }
     }
 
     private fun checkUsernameAvailability(
         username: String,
         email: String,
-        password: String,
-        context: Context
+        password: String
     ) {
         firestore.collection("users").whereEqualTo("username", username).get()
             .addOnSuccessListener { documents ->
                 if (!documents.isEmpty) {
-                    _registrationStatus.value = context.getString(R.string.username_already_error)
+                    _registrationStatus.value = R.string.username_already_error
                 } else {
-                    createUser(email, password, username, context)
+                    createUser(email, password, username)
                 }
             }
-            .addOnFailureListener { exception ->
-                _registrationStatus.value =
-                    context.getString(R.string.error_checking_username, exception.message)
+            .addOnFailureListener {
+                _registrationStatus.value = R.string.error_checking_username
             }
     }
 
-    private fun createUser(email: String, password: String, username: String, context: Context) {
+    private fun createUser(email: String, password: String, username: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val userId = auth.currentUser?.uid
-                    saveUserToFirestore(username, userId, context)
+                    saveUserToFirestore(username, userId)
                 } else {
-                    _registrationStatus.value =
-                        context.getString(R.string.registration_error, task.exception?.message)
+                    _registrationStatus.value = R.string.registration_error
                 }
             }
     }
 
-    private fun saveUserToFirestore(username: String, userId: String?, context: Context) {
+    private fun saveUserToFirestore(username: String, userId: String?) {
         val user = User(username, userId)
         userId?.let {
             firestore.collection("users").document(it).set(user)
                 .addOnSuccessListener {
-                    _registrationStatus.value = context.getString(R.string.registration_successful)
+                    _registrationStatus.value = R.string.registration_successful
                 }
-                .addOnFailureListener { e ->
-                    _registrationStatus.value =
-                        context.getString(R.string.error_saving_user_data, e.message)
+                .addOnFailureListener {
+                    _registrationStatus.value = R.string.error_saving_user_data
                 }
         }
     }
