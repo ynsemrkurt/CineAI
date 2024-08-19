d33ed347201835d35232ba899e4dd3041634e22f
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-19 14:13:29 +0300

    feat: Add Gemini dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,7 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+
+    //Gemini
+    implementation(libs.generativeai)
 }
