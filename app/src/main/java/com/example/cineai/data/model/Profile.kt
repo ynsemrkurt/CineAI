ed4b26e412f170fab1037bfa343eda0fe0cdb559
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-19 12:58:35 +0300

    feat: Add Profile data class

---
+++ app/src/main/java/com/example/cineai/data/model/Profile.kt
@@ -0,0 +1,12 @@
+package com.example.cineai.data.model
+
+data class Profile(
+    val stress: String,
+    val problemSolving: String,
+    val decisionMaking: String,
+    val teamwork: String,
+    val movieGenres: String,
+    val music: String,
+    val hobbies: String,
+    val travel: String
+)
