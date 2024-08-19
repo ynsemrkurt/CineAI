e1168470f9986eca83bd7a05b31f41534c503b25
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-19 12:59:05 +0300

    feat: Check user profile on login

--- app/src/main/java/com/example/cineai/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/cineai/ui/activity/LoginActivity.kt
@@ -2,13 +2,16 @@ package com.example.cineai.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cineai.R
+import com.example.cineai.ui.viewmodel.LoginViewModel
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +19,23 @@ class LoginActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
         if (auth.currentUser != null) {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            loginViewModel.checkProfile()
+            observeLoginStatus()
+        }
+    }
+
+    private fun observeLoginStatus() {
+        loginViewModel.loginStatus.observe(this) { status ->
+            when (status) {
+                R.string.login_successful -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
+
+                R.string.profile_missing -> {
+                    auth.signOut()
+                }
+            }
         }
     }
 }
