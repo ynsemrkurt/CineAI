ca860d6b0d1d4e31a2401b49ba2cd1cf976ba60e
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-19 12:55:50 +0300

    feat: Add extension function to open fragments with animations

--- app/src/main/java/com/example/cineai/ui/classes/Extensions.kt
+++ app/src/main/java/com/example/cineai/ui/classes/Extensions.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.cineai.R
 
 fun Fragment.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(requireContext(), message, duration).show()
@@ -27,4 +28,17 @@ fun <T : Parcelable> Fragment.putParcelable(key: String, value: T) {
 
 fun String.isValidEmail(): Boolean {
     return this.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+}
+
+fun Fragment.openFragment(containerViewId: Int, fragment: Fragment) {
+    parentFragmentManager.beginTransaction()
+        .setCustomAnimations(
+            R.anim.slide_in_right,
+            R.anim.slide_out_left,
+            R.anim.slide_in_left,
+            R.anim.slide_out_right
+        )
+        .replace(containerViewId, fragment)
+        .addToBackStack(null)
+        .commit()
 }
