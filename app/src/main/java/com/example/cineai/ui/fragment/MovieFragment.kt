0cc1f0a489793fd18002a3c1903658044eb909df
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-19 12:56:38 +0300

    refactor: Use MoviePagerAdapter in MovieFragment

--- app/src/main/java/com/example/cineai/ui/fragment/MovieFragment.kt
+++ app/src/main/java/com/example/cineai/ui/fragment/MovieFragment.kt
@@ -5,11 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.cineai.R
 import com.example.cineai.databinding.FragmentMovieBinding
-import com.example.cineai.ui.classes.MovieCategory
+import com.example.cineai.ui.adapter.MoviePagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MovieFragment : Fragment() {
@@ -40,21 +38,5 @@ class MovieFragment : Fragment() {
                 else -> null
             }
         }.attach()
-    }
-
-    class MoviePagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
-        override fun getItemCount(): Int {
-            return 4
-        }
-
-        override fun createFragment(position: Int): Fragment {
-            return when (position) {
-                0 -> BaseMovieFragment.newInstance(MovieCategory.POPULAR)
-                1 -> BaseMovieFragment.newInstance(MovieCategory.TOP_RATED)
-                2 -> BaseMovieFragment.newInstance(MovieCategory.NOW_PLAYING)
-                3 -> BaseMovieFragment.newInstance(MovieCategory.UPCOMING)
-                else -> BaseMovieFragment.newInstance(MovieCategory.POPULAR)
-            }
-        }
     }
 }
