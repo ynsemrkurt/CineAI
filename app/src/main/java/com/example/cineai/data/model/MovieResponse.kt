f014373807ca6cceba581a2b4a426b1f5f42e264
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-21 19:37:42 +0300

    feat: Add VideoResponse and Video data classes

--- app/src/main/java/com/example/cineai/data/model/MovieResponse.kt
+++ app/src/main/java/com/example/cineai/data/model/MovieResponse.kt
@@ -7,6 +7,18 @@ data class MovieResponse(
     val results: List<Movie>
 )
 
+data class VideoResponse(
+    @SerializedName("results")
+    val results: List<Video>
+)
+
+data class Video(
+    @SerializedName("key")
+    val key: String,
+    @SerializedName("type")
+    val type: String,
+)
+
 data class Movie(
     @SerializedName("id")
     val id: Int,
@@ -18,4 +30,19 @@ data class Movie(
     val voteAverage: Double,
     @SerializedName("overview")
     val overview: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
+    @SerializedName("genres")
+    val genreIds: List<Genre>,
+    @SerializedName("status")
+    val status: String,
+)
+
+data class Genre(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
 )
