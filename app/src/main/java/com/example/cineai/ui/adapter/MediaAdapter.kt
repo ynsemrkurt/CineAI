d89a300758e33069abe929730d7b1ef426d47769
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-28 16:07:14 +0300

    refactor: Migrate YouTubeViewHolder to ViewBinding

--- app/src/main/java/com/example/cineai/ui/adapter/MediaAdapter.kt
+++ app/src/main/java/com/example/cineai/ui/adapter/MediaAdapter.kt
@@ -4,17 +4,16 @@ import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cineai.R
+import com.example.cineai.databinding.ItemImageViewBinding
+import com.example.cineai.databinding.ItemYoutubePlayerBinding
 import com.example.cineai.ui.classes.FullScreenHelper
 import com.example.cineai.ui.classes.loadImage
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.FullscreenListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class MediaAdapter(
     private val items: List<ItemType>,
@@ -37,19 +36,12 @@ class MediaAdapter(
         val inflater = LayoutInflater.from(parent.context)
         return when (viewType) {
             TYPE_YOUTUBE_PLAYER -> YouTubeViewHolder(
-                inflater.inflate(
-                    R.layout.item_youtube_player,
-                    parent,
-                    false
-                ), lifecycle
+                ItemYoutubePlayerBinding.inflate(inflater, parent, false),
+                lifecycle
             )
 
             TYPE_IMAGE_VIEW -> ImageViewHolder(
-                inflater.inflate(
-                    R.layout.item_image_view,
-                    parent,
-                    false
-                )
+                ItemImageViewBinding.inflate(inflater, parent, false)
             )
 
             else -> throw IllegalArgumentException("Invalid view type")
@@ -65,13 +57,14 @@ class MediaAdapter(
 
     override fun getItemCount(): Int = items.size
 
-    class YouTubeViewHolder(itemView: View, lifecycle: Lifecycle) :
-        RecyclerView.ViewHolder(itemView) {
-        private val youTubePlayerView: YouTubePlayerView = itemView.findViewById(R.id.youtubePlayer)
+    class YouTubeViewHolder(
+        private val binding: ItemYoutubePlayerBinding,
+        lifecycle: Lifecycle
+    ) : RecyclerView.ViewHolder(binding.root) {
         private var youTubePlayer: YouTubePlayer? = null
 
         init {
-            lifecycle.addObserver(youTubePlayerView)
+            lifecycle.addObserver(binding.youtubePlayer)
         }
 
         fun bind(item: ItemType.YouTube) {
@@ -88,14 +81,14 @@ class MediaAdapter(
                 .fullscreen(1)
                 .build()
 
-            youTubePlayerView.initialize(object : AbstractYouTubePlayerListener() {
+            binding.youtubePlayer.initialize(object : AbstractYouTubePlayerListener() {
                 override fun onReady(youTubePlayer: YouTubePlayer) {
                     this@YouTubeViewHolder.youTubePlayer = youTubePlayer
                     youTubePlayer.cueVideo(videoId, 0f)
 
                     val activity = itemView.context as Activity
 
-                    youTubePlayerView.addFullscreenListener(object : FullscreenListener {
+                    binding.youtubePlayer.addFullscreenListener(object : FullscreenListener {
                         override fun onEnterFullscreen(
                             fullscreenView: View,
                             exitFullscreen: () -> Unit
@@ -112,11 +105,12 @@ class MediaAdapter(
         }
     }
 
-    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val imageView: ImageView = itemView.findViewById(R.id.imageViewBackdrop)
+    class ImageViewHolder(
+        private val binding: ItemImageViewBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: ItemType.Image) {
-            imageView.loadImage("https://image.tmdb.org/t/p/original${item.imageUrl}")
+            binding.imageViewBackdrop.loadImage("https://image.tmdb.org/t/p/original${item.imageUrl}")
         }
     }
 }
