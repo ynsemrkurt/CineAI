67ed6ba1dd266f03be1117f8dca3dfc7690d2bd7
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-08-28 18:14:17 +0300

    refactor: Use Dialog for fullscreen view

--- app/src/main/java/com/example/cineai/ui/classes/FullScreenHelper.kt
+++ app/src/main/java/com/example/cineai/ui/classes/FullScreenHelper.kt
@@ -1,31 +1,37 @@
 package com.example.cineai.ui.classes
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.pm.ActivityInfo
 import android.view.View
-import android.widget.FrameLayout
+import android.view.Window
 import androidx.viewpager2.widget.ViewPager2
 import com.example.cineai.R
 
 object FullScreenHelper {
 
+    private var dialog: Dialog? = null
+
     fun enterFullScreen(activity: Activity, fullscreenView: View) {
-        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        activity.hideSystemUI()
-        val fullScreenLayout = activity.findViewById<FrameLayout>(R.id.fullScreenLayout)
         val viewPager2 = activity.findViewById<ViewPager2>(R.id.viewPagerMovie)
         viewPager2.visibility = View.GONE
-        fullScreenLayout.visibility = View.VISIBLE
-        fullScreenLayout.addView(fullscreenView)
+
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+        dialog = Dialog(activity).apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setContentView(fullscreenView)
+            window?.hideSystemUI()
+        }
+
+        dialog?.show()
     }
 
     fun exitFullScreen(activity: Activity) {
-        val fullScreenLayout = activity.findViewById<FrameLayout>(R.id.fullScreenLayout)
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        dialog?.dismiss()
+
         val viewPager2 = activity.findViewById<ViewPager2>(R.id.viewPagerMovie)
         viewPager2.visibility = View.VISIBLE
-        fullScreenLayout.visibility = View.GONE
-        fullScreenLayout.removeAllViews()
-        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
-        activity.showSystemUI()
     }
 }
