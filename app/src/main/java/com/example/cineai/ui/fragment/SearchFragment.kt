72b7839a27292a805dd39a67be8e938c1434d51c
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-09-01 07:47:02 +0300

    feat: Add SearchFragment

---
+++ app/src/main/java/com/example/cineai/ui/fragment/SearchFragment.kt
@@ -0,0 +1,22 @@
+package com.example.cineai.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cineai.R
+import com.example.cineai.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater)
+        return binding.root
+    }
+}
