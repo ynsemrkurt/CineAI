8b05df2ddf8a65251e11769f66e336e3d42bac13
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-09-05 15:38:43 +0300

    feat: Add image download functionality to ImageDialogFragment

--- app/src/main/java/com/example/cineai/ui/fragment/ImageDialogFragment.kt
+++ app/src/main/java/com/example/cineai/ui/fragment/ImageDialogFragment.kt
@@ -1,12 +1,18 @@
 package com.example.cineai.ui.fragment
 
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.bumptech.glide.Glide
+import com.example.cineai.R
 import com.example.cineai.databinding.FragmentImageDialogBinding
+import com.example.cineai.ui.classes.showToast
 
 class ImageDialogFragment : DialogFragment() {
 
@@ -38,6 +44,9 @@ class ImageDialogFragment : DialogFragment() {
 
         // Download button
         binding.imageButtonDownload.setOnClickListener {
+            imageUrl?.let {
+                downloadImage(it, it.substringAfterLast("/"))
+            }
         }
     }
 
@@ -49,5 +58,18 @@ class ImageDialogFragment : DialogFragment() {
 
     fun setImageUrl(url: String) {
         this.imageUrl = url
+    }
+
+    private fun downloadImage(imageUrl: String, fileName: String) {
+        val request = DownloadManager.Request(Uri.parse(imageUrl))
+            .setTitle(getString(R.string.downloading_image))
+            .setDescription(getString(R.string.downloading_image_from, imageUrl))
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "$fileName.jpg")
+
+        val downloadManager = requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        downloadManager.enqueue(request)
+
+        requireContext().showToast(getString(R.string.image_downloading))
     }
 }
