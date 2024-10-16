db3bd8e26415ac9b852e1de3330aa74c9547eefc
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-10-16 20:43:40 +0300

    refactor: Simplify MovieCategory usage in MoviePagerAdapter

--- app/src/main/java/com/example/cineai/ui/adapter/MoviePagerAdapter.kt
+++ app/src/main/java/com/example/cineai/ui/adapter/MoviePagerAdapter.kt
@@ -3,7 +3,7 @@ package com.example.cineai.ui.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.cineai.ui.classes.MovieCategory
+import com.example.cineai.ui.classes.MovieCategory.*
 import com.example.cineai.ui.fragment.BaseMovieFragment
 
 class MoviePagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
@@ -14,11 +14,11 @@ class MoviePagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activ
     override fun createFragment(position: Int): Fragment {
         return BaseMovieFragment.newInstance(
             when (position) {
-                0 -> MovieCategory.POPULAR
-                1 -> MovieCategory.TOP_RATED
-                2 -> MovieCategory.NOW_PLAYING
-                3 -> MovieCategory.UPCOMING
-                else -> MovieCategory.POPULAR
+                0 -> POPULAR
+                1 -> TOP_RATED
+                2 -> NOW_PLAYING
+                3 -> UPCOMING
+                else -> POPULAR
             }
         )
     }
